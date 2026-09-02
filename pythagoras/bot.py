aaf4ac620a7215aab7a05099607f036bbc9df57e
amri4 <amri.supreme123@gmail.com>
2026-09-03 02:55:17 +0300

    update pythagoras/bot.py

---
+++ pythagoras/bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+from pathlib import Path
+from utils.help_command import help_command
+
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        self.bot_name = Path(__file__).parent.name
+
+        super().__init__(
+            command_prefix="Pythagoras ",
+            intents=discord.Intents.all(),
+            help_command=help_command
+        )
+
+    async def on_ready(self):
+        print(f"[{self.bot_name}] Logged in as {self.user}")
+
+
+bot = Bot()
