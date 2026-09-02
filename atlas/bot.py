abb57e7387e7e37d9ca3120d0feb62a3dfe2b909
amri4 <amri.supreme123@gmail.com>
2026-09-02 22:59:50 +0300

    update atlas/bot.py

--- atlas/bot.py
+++ atlas/bot.py
@@ -1,15 +1,22 @@
 import discord
 from discord.ext import commands
 from pathlib import Path
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 
 class Bot(commands.Bot):
 
     def __init__(self):
         self.bot_name = Path(__file__).parent.name
+        self.token = os.getenv("ATLAS_TOKEN")
 
         super().__init__(
-            command_prefix="Atlas ",
+            command_prefix=f"{self.bot_name} ",
             intents=discord.Intents.all()
         )
 
