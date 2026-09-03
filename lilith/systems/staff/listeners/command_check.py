7a18c4351411822de38043c13629c39ba9392f8e
amri4 <amri.supreme123@gmail.com>
2026-09-03 13:01:26 +0300

    update lilith/systems/staff/listeners/command_check.py

---
+++ lilith/systems/staff/listeners/command_check.py
@@ -0,0 +1,38 @@
+import discord
+
+from ..functions.check_command_rank import check_command_rank
+from ..functions.get_command_rank import get_command_rank
+
+
+async def on_command(ctx):
+
+    if ctx.guild is None:
+        return
+
+    required = get_command_rank(
+        ctx.guild,
+        ctx.command.name
+    )
+
+    if required is None:
+        return
+
+    if check_command_rank(
+        ctx.author,
+        ctx.command.name
+    ):
+        return
+
+    await ctx.send(
+        f"❌ You need **{required['name']}** "
+        f"or above to use this command."
+    )
+
+    ctx.command.reset_cooldown(ctx)
+
+
+def setup(bot):
+    bot.add_listener(
+        on_command,
+        "on_command"
+    )
