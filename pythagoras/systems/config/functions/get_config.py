a9477769dabbec061f1362167a64c7b1e88938c1
amri4 <amri.supreme123@gmail.com>
2026-09-03 13:27:21 +0300

    update pythagoras/systems/config/functions/get_config.py

---
+++ pythagoras/systems/config/functions/get_config.py
@@ -0,0 +1,32 @@
+import mycord
+
+
+db = mycord.PunksDB()
+
+
+def get_config(guild):
+
+    if guild is None:
+        return None
+
+    config = db.fetchone(
+        "server_config",
+        "guild_id = ?",
+        (guild.id,)
+    )
+
+    if config is None:
+
+        db.insert(
+            "server_config",
+            "guild_id",
+            (guild.id,)
+        )
+
+        config = db.fetchone(
+            "server_config",
+            "guild_id = ?",
+            (guild.id,)
+        )
+
+    return config
