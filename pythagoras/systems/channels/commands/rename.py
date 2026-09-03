ca29fcfed819b094d58525d49a569e8a4824fde9
amri4 <amri.supreme123@gmail.com>
2026-09-03 03:54:23 +0300

    update pythagoras/systems/channels/commands/rename.py

--- pythagoras/systems/channels/commands/rename.py
+++ pythagoras/systems/channels/commands/rename.py
@@ -1,24 +1,27 @@
 import discord
 from discord.ext import commands
 
+from ..functions.find_channel import find_channel
+
 
 @commands.command(
     name="renamechannel",
     help="Rename a channel."
 )
-async def renamechannel(
-    ctx,
-    channel: discord.TextChannel = None,
-    *,
-    name: str = None
-):
+async def renamechannel(ctx, channel_input: str = None, *, name: str = None):
 
-    if channel is None:
+    if not channel_input:
         await ctx.send("❌ Please specify a channel.")
         return
 
     if not name:
         await ctx.send("❌ Please specify a new name.")
+        return
+
+    channel = find_channel(ctx.guild, channel_input)
+
+    if channel is None:
+        await ctx.send("❌ Channel not found.")
         return
 
     old_name = channel.name
