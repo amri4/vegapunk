ee7dff9ebabd9d4121eb3a85c3d2d42649136a50
amri4 <amri.supreme123@gmail.com>
2026-09-03 03:54:53 +0300

    update pythagoras/systems/channels/commands/del.py

--- pythagoras/systems/channels/commands/del.py
+++ pythagoras/systems/channels/commands/del.py
@@ -1,15 +1,23 @@
 import discord
 from discord.ext import commands
 
+from ..functions.find_channel import find_channel
+
 
 @commands.command(
     name="delchannel",
     help="Delete a channel."
 )
-async def delchannel(ctx, channel: discord.TextChannel = None):
+async def delchannel(ctx, channel_input: str = None):
+
+    if not channel_input:
+        await ctx.send("❌ Please specify a channel.")
+        return
+
+    channel = find_channel(ctx.guild, channel_input)
 
     if channel is None:
-        await ctx.send("❌ Please specify a channel.")
+        await ctx.send("❌ Channel not found.")
         return
 
     channel_name = channel.name
