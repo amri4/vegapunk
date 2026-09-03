9e8ade79c971456c57e266a1bbdf77a4614c2631
amri4 <amri.supreme123@gmail.com>
2026-09-03 13:06:51 +0300

    update lilith/systems/staff/listeners/role_delete.py

--- lilith/systems/staff/listeners/role_delete.py
+++ lilith/systems/staff/listeners/role_delete.py
@@ -6,6 +6,30 @@ db = mycord.PunksDB()
 
 async def on_guild_role_delete(role):
 
+    # Find the staff rank using this role
+    rank = db.fetchone(
+        "staff_ranks",
+        "guild_id = ? AND role_id = ?",
+        (
+            role.guild.id,
+            role.id
+        )
+    )
+
+    if rank is None:
+        return
+
+    # Remove command requirements using this rank
+    db.delete(
+        "command_ranks",
+        "guild_id = ? AND required_level = ?",
+        (
+            role.guild.id,
+            rank["level"]
+        )
+    )
+
+    # Remove the staff rank itself
     db.delete(
         "staff_ranks",
         "guild_id = ? AND role_id = ?",
