72f7fc145623b303bfab6aada994346bdbf5941d
amri4 <amri.supreme123@gmail.com>
2026-09-03 13:28:59 +0300

    update pythagoras/systems/config/commands/setmemberrole.py

---
+++ pythagoras/systems/config/commands/setmemberrole.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+
+import mycord
+
+
+db = mycord.PunksDB()
+
+
+@commands.command(
+    name="setmemberrole",
+    help="Set the role given to verified members."
+)
+async def setmemberrole(
+    ctx,
+    role: discord.Role
+):
+
+    db.update(
+        "server_config",
+        "member_role_id = ?",
+        "guild_id = ?",
+        (
+            role.id,
+            ctx.guild.id
+        )
+    )
+
+    await ctx.send(
+        f"✅ The member role is now {role.mention}."
+    )
+
+
+def setup(bot):
+    bot.add_command(setmemberrole)
