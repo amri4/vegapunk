3278e57f3f56843db46ad392a6697dd8b905408e
amri4 <amri.supreme123@gmail.com>
2026-09-04 03:00:07 +0300

    update lilith/systems/staff/functions/get_ranks.py

---
+++ lilith/systems/staff/functions/get_ranks.py
@@ -0,0 +1,17 @@
+import mycord
+
+
+db = mycord.PunksDB()
+
+
+def get_ranks(guild):
+
+    rows = db.fetchall(
+        "staff_ranks"
+    )
+
+    return [
+        row
+        for row in rows
+        if row[0] == guild.id
+    ]
