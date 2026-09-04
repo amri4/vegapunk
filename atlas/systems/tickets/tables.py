e975d48d7f72ea628026ab49abcc8f82a89166bd
amri4 <amri.supreme123@gmail.com>
2026-09-04 23:58:10 +0300

    update atlas/systems/tickets/tables.py

---
+++ atlas/systems/tickets/tables.py
@@ -0,0 +1,68 @@
+import mycord
+
+
+db = mycord.PunksDB()
+
+
+# =========================================
+# TICKET PANELS
+# =========================================
+
+db.create_table(
+    "ticket_panels",
+    """
+    panel_id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    guild_id INTEGER,
+    message_id INTEGER,
+
+    title TEXT,
+    description TEXT,
+
+    image_url TEXT,
+    thumbnail_url TEXT
+    """
+)
+
+
+# =========================================
+# TICKET TYPES
+# =========================================
+
+db.create_table(
+    "ticket_types",
+    """
+    ticket_type_id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    guild_id INTEGER,
+    panel_id INTEGER,
+
+    name TEXT,
+    color TEXT,
+
+    ticket_message TEXT,
+    message_image_url TEXT
+    """
+)
+
+
+# =========================================
+# TICKETS
+# =========================================
+
+db.create_table(
+    "tickets",
+    """
+    ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+    guild_id INTEGER,
+
+    panel_id INTEGER,
+    ticket_type_id INTEGER,
+
+    channel_id INTEGER,
+    member_id INTEGER,
+
+    status TEXT
+    """
+)
