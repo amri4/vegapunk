b62d3ade67adad4e79f1c41680dc0ada7373da7c
amri4 <amri.supreme123@gmail.com>
2026-09-04 13:23:43 +0300

    update pythagoras/systems/config/tables.py

--- pythagoras/systems/config/tables.py
+++ pythagoras/systems/config/tables.py
@@ -8,8 +8,13 @@ db.create_table(
     "server_config",
     """
     guild_id INTEGER PRIMARY KEY,
+
     member_role_id INTEGER,
+
     verification_channel_id INTEGER,
-    verification_enabled INTEGER
+    verification_enabled INTEGER,
+
+    ticket_panel_channel_id INTEGER,
+    ticket_category_id INTEGER
     """
 )
