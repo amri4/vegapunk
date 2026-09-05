42b96b04f7c77addaa0be166f9c974fc0159bb27
amri4 <amri.supreme123@gmail.com>
2026-09-05 03:07:34 +0300

    update atlas/systems/tickets/commands/createpanel.py

--- atlas/systems/tickets/commands/createpanel.py
+++ atlas/systems/tickets/commands/createpanel.py
@@ -14,7 +14,21 @@ db = mycord.PunksDB()
     help="create ticket pannel"
 )
 async def createpanel(ctx):
-    await ctx.send("Command worked")
+   
+    await ctx.send("What title do you want for the pannel?")
+    message = await get_message(ctx)
+    title = message.content
+
+    await ctx.send("Add discription")
+    message = await get_message(ctx)
+    description = message.content
+
+    embed = discord.Embed(
+        title=f"{title}",
+        description=f"{description}",
+        color=discord.Color.blue()
+    )
+    await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_command(createpanel)
