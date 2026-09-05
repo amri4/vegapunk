584121d9c0e5e5fbc65be180f7e65672d4c306a7
amri4 <amri.supreme123@gmail.com>
2026-09-05 04:26:53 +0300

    update pythagoras/systems/config/tables.py

--- pythagoras/systems/config/tables.py
+++ pythagoras/systems/config/tables.py
@@ -1,20 +1,16 @@
 import mycord
 
-
 db = mycord.PunksDB()
 
+db.drop_table("server_config")
 
 db.create_table(
     "server_config",
     """
     guild_id INTEGER PRIMARY KEY,
-
     member_role_id INTEGER,
-
     verification_channel_id INTEGER,
     verification_enabled INTEGER,
-
-    ticket_panel_channel_id INTEGER,
-    ticket_category_id INTEGER
+    ticket_panel_channel_id INTEGER
     """
 )
