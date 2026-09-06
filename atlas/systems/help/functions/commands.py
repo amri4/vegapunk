48a1a3769f94611c9dbc7ce4d72cedb5409df300
amri4 <amri.supreme123@gmail.com>
2026-09-07 01:37:17 +0300

    update atlas/systems/help/functions/commands.py

--- atlas/systems/help/functions/commands.py
+++ atlas/systems/help/functions/commands.py
@@ -1,33 +1,12 @@
 from discord import app_commands
-
 from .categories import get_category
-
-
-def get_prefix_commands(bot):
-    categories = {}
-
-    for command in bot.commands:
-
-        if command.name == "help":
-            continue
-
-        category = get_category(command.callback)
-
-        categories.setdefault(category, [])
-        categories[category].append(command)
-
-    return dict(sorted(categories.items()))
 
 
 def get_slash_commands(bot):
     categories = {}
 
     for command in bot.tree.get_commands():
-        
-        print("NAME:", command.name)
-        print("CALLBACK:", command.callback)
-        print("FILE:", getattr(command.callback, "__code__", None).co_filename)
-    
+
         if isinstance(command, app_commands.Group):
             continue
 
@@ -38,6 +17,5 @@ def get_slash_commands(bot):
 
         categories.setdefault(category, [])
         categories[category].append(command)
-    print("SLASH COMMANDS:", bot.tree.get_commands())
 
     return dict(sorted(categories.items()))
