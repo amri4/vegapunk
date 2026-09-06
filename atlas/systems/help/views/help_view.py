7bdedc8a4d75f6a4bc707fa51746b082924a94c1
amri4 <amri.supreme123@gmail.com>
2026-09-07 02:56:05 +0300

    update atlas/systems/help/views/help_view.py

--- atlas/systems/help/views/help_view.py
+++ atlas/systems/help/views/help_view.py
@@ -4,18 +4,24 @@ from ..functions.commands import (
     get_prefix_commands,
     get_slash_commands
 )
+
 from ..functions.embed import build_help_embed
+
 from ..selects.command_type import CommandTypeSelect
-from ..buttons.navigation import PreviousButton, NextButton
+
+from ..buttons.navigation import (
+    PreviousButton,
+    NextButton
+)
 
 
 class HelpView(discord.ui.View):
-
     def __init__(self, bot, author):
         super().__init__(timeout=180)
 
         self.bot = bot
         self.author = author
+
         self.mode = "slash"
         self.page = 0
 
@@ -31,13 +37,19 @@ class HelpView(discord.ui.View):
     def refresh(self):
         if self.mode == "prefix":
             self.categories = get_prefix_commands(self.bot)
+
         else:
             self.categories = get_slash_commands(self.bot)
 
-        self.category_names = list(self.categories.keys())
+        self.category_names = list(
+            self.categories.keys()
+        )
 
         if self.page >= len(self.category_names):
-            self.page = max(0, len(self.category_names) - 1)
+            self.page = max(
+                0,
+                len(self.category_names) - 1
+            )
 
         self.update_buttons()
 
