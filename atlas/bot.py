8a4904b9caf7da8ba50fe8b034d085369c5173e3
amri4 <amri.supreme123@gmail.com>
2026-09-06 17:37:23 +0300

    update atlas/bot.py

--- atlas/bot.py
+++ atlas/bot.py
@@ -2,7 +2,6 @@ import os
 import discord
 from discord.ext import commands
 from pathlib import Path
-from utils.help_command import help_command
 
 
 class Bot(commands.Bot):
@@ -13,24 +12,10 @@ class Bot(commands.Bot):
         super().__init__(
             command_prefix="Atlas ",
             intents=discord.Intents.all(),
-            help_command=help_command,
             application_id=int(os.getenv("ATLAS_APPLICATION_ID"))
         )
 
     async def on_ready(self):
-
-        try:
-            synced = await self.tree.sync()
-
-            print(
-                f"[{self.bot_name}] Synced {len(synced)} slash commands"
-            )
-
-        except Exception as error:
-            print(
-                f"[{self.bot_name}] Failed to sync slash commands: {error}"
-            )
-
         print(f"[{self.bot_name}] Logged in as {self.user}")
 
 
