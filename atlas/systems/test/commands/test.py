4f4249e8ba389bd9379c4aa8f60489045e1ab833
amri4 <amri.supreme123@gmail.com>
2026-09-06 17:10:56 +0300

    update atlas/systems/test/commands/test.py

--- atlas/systems/test/commands/test.py
+++ atlas/systems/test/commands/test.py
@@ -1,9 +1,23 @@
-from discord.ext import commands
+import discord
+from discord import app_commands
 
 
-@commands.command()
-async def test(ctx):
-    await ctx.send("Atlas systems are working!")
+@app_commands.command(
+    name="ping",
+    description="Check if the bot is online"
+)
+async def ping(interaction: discord.Interaction):
+
+    embed = discord.Embed(
+        title="Pong! 🏓",
+        description="The bot is online.",
+        color=discord.Color.blurple()
+    )
+
+    await interaction.response.send_message(
+        embed=embed
+    )
+
 
 def setup(bot):
-    bot.add_command(test)
+    bot.tree.add_command(ping)
