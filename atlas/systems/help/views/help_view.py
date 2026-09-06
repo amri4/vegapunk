6f44505add965b604c2dd102b24d19280bccba88
amri4 <amri.supreme123@gmail.com>
2026-09-07 01:36:40 +0300

    update atlas/systems/help/views/help_view.py

--- atlas/systems/help/views/help_view.py
+++ atlas/systems/help/views/help_view.py
@@ -1,65 +1,41 @@
 import discord
 
-from ..functions.commands import (
-    get_prefix_commands,
-    get_slash_commands
-)
-
+from ..functions.commands import get_slash_commands
 from ..functions.embed import build_help_embed
-
-from ..selects.command_type import CommandTypeSelect
 from ..buttons.navigation import PreviousButton, NextButton
 
 
 class HelpView(discord.ui.View):
 
     def __init__(self, bot, author):
-
         super().__init__(timeout=180)
 
         self.bot = bot
         self.author = author
-
-        self.mode = "prefix"
         self.page = 0
 
-        self.categories = {}
-        self.category_names = []
+        self.categories = get_slash_commands(self.bot)
+        self.category_names = list(self.categories.keys())
 
-        self.add_item(CommandTypeSelect(self))
         self.add_item(PreviousButton(self))
         self.add_item(NextButton(self))
 
-        self.refresh()
-
     def refresh(self):
-
-        if self.mode == "prefix":
-            self.categories = get_prefix_commands(self.bot)
-        else:
-            self.categories = get_slash_commands(self.bot)
-
-        self.category_names = list(
-            self.categories.keys()
-        )
+        self.categories = get_slash_commands(self.bot)
+        self.category_names = list(self.categories.keys())
 
         if self.page >= len(self.category_names):
-            self.page = max(
-                0,
-                len(self.category_names) - 1
-            )
+            self.page = max(0, len(self.category_names) - 1)
 
     def embed(self):
-
         return build_help_embed(
             self.bot,
-            self.mode,
+            "slash",
             self.categories,
             self.category_names,
             self.page
         )
 
     async def on_timeout(self):
-
         for item in self.children:
             item.disabled = True
