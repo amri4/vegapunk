b507e88a5b8b41e88df6db203ab222521fe2f75c
amri4 <amri.supreme123@gmail.com>
2026-09-07 03:09:34 +0300

    update atlas/systems/help/views/help_view.py

--- atlas/systems/help/views/help_view.py
+++ atlas/systems/help/views/help_view.py
@@ -1,14 +1,7 @@
 import discord
 
-from ..functions.commands import (
-    get_prefix_commands,
-    get_slash_commands
-)
-
+from ..functions.commands import get_slash_commands
 from ..functions.embed import build_help_embed
-
-from ..selects.command_type import CommandTypeSelect
-
 from ..buttons.navigation import (
     PreviousButton,
     NextButton
@@ -22,25 +15,20 @@ class HelpView(discord.ui.View):
         self.bot = bot
         self.author = author
 
-        self.mode = "slash"
         self.page = 0
 
-        self.categories = {}
-        self.category_names = []
+        self.categories = get_slash_commands(bot)
+        self.category_names = list(
+            self.categories.keys()
+        )
 
-        self.add_item(CommandTypeSelect(self))
         self.add_item(PreviousButton(self))
         self.add_item(NextButton(self))
 
-        self.refresh()
+        self.update_buttons()
 
     def refresh(self):
-        if self.mode == "prefix":
-            self.categories = get_prefix_commands(self.bot)
-
-        else:
-            self.categories = get_slash_commands(self.bot)
-
+        self.categories = get_slash_commands(self.bot)
         self.category_names = list(
             self.categories.keys()
         )
@@ -63,8 +51,7 @@ class HelpView(discord.ui.View):
             self.bot,
             self.categories,
             self.category_names,
-            self.page,
-            self.mode
+            self.page
         )
 
     async def on_timeout(self):
