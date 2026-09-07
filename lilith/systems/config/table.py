1a866ad68a061efa59a985060e31f762627f696e
amri4 <amri.supreme123@gmail.com>
2026-09-07 23:14:35 +0300

    update lilith/systems/config/table.py

--- lilith/systems/config/table.py
+++ lilith/systems/config/table.py
@@ -4,11 +4,13 @@ import mycord
 db = mycord.DB()
 
 
- db.create_table(
-    "verification_config",
-    """
-    guild_id INTEGER PRIMARY KEY,
-    verified_role_id INTEGER,
-    enabled INTEGER NOT NULL DEFAULT 0
-    """
-)
+def setup():
+    db.create_table(
+        "verification_config",
+        """
+        guild_id INTEGER PRIMARY KEY,
+        verified_role_id INTEGER,
+        verification_channel_id INTEGER,
+        enabled INTEGER NOT NULL DEFAULT 0
+        """
+    )
