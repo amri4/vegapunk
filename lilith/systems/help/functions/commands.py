200ad1225196ecb1a534a9ae22d6c72ef039ca53
amri4 <amri.supreme123@gmail.com>
2026-09-07 13:37:54 +0300

    create lilith/systems/help/functions/commands.py

---
+++ lilith/systems/help/functions/commands.py
@@ -0,0 +1,20 @@
+from discord import app_commands
+from .categories import get_category
+
+
+def get_slash_commands(bot):
+    categories = {}
+
+    for command in bot.tree.get_commands():
+        if isinstance(command, app_commands.Group):
+            continue
+
+        if command.name == "help":
+            continue
+
+        category = get_category(command.callback)
+
+        categories.setdefault(category, [])
+        categories[category].append(command)
+
+    return dict(sorted(categories.items()))
