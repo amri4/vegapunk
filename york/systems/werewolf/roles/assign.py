b82c3b613e9535a44cb686504e173ea3bd0bcdbb
amri4 <amri.supreme123@gmail.com>
2026-09-09 00:46:35 +0300

    update york/systems/werewolf/roles/assign.py

--- york/systems/werewolf/roles/assign.py
+++ york/systems/werewolf/roles/assign.py
@@ -20,3 +20,11 @@ def assign_roles(
     )
 
     random.shuffle(roles)
+    for player, role in zip(players, roles):
+        db.update(
+            "werewolf_players",
+            "role = ?",
+            "id = ?",
+            (role, player[0])
+        )
+    return True
