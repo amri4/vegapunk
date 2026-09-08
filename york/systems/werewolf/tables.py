86f11c30acb368ec51be49f819740786e966c28d
amri4 <amri.supreme123@gmail.com>
2026-09-08 23:30:56 +0300

    update york/systems/werewolf/tables.py

--- york/systems/werewolf/tables.py
+++ york/systems/werewolf/tables.py
@@ -22,8 +22,8 @@ db.create_table(
     game_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     is_bot INTEGER NOT NULL DEFAULT 0,
-    display_name TEXT,
     role TEXT,
-    alive INTEGER NOT NULL DEFAULT 1
+    alive INTEGER NOT NULL DEFAULT 1,
+    display_name TEXT
     """
 )
