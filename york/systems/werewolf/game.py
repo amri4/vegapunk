0df43eac2de935d3cb61126111025569f57a2527
amri4 <amri.supreme123@gmail.com>
2026-09-09 00:52:21 +0300

    update york/systems/werewolf/game.py

--- york/systems/werewolf/game.py
+++ york/systems/werewolf/game.py
@@ -1,5 +1,7 @@
 import mycord
 
+from .roles.assign import assign_roles
+
 db = mycord.DB()
 
 
@@ -20,16 +22,29 @@ def start_game(game_id):
             "game_id, is_bot, display_name",
             (game_id, 1, f"York Bot {i + 1}")
         )
-        db.update(
-            "werewolf_games",
-            "status = ?",
-            "id = ?",
-            ("active", game_id)
-        )
-        db.update(
-            "werewolf_games",
-            "phase = ?",
-            "id = ?",
-            ("night", game_id)
-        )
+    db.update(
+        "werewolf_games",
+        "status = ?",
+        "id = ?",
+        ("active", game_id)
+    )
+    db.update(
+        "werewolf_games",
+        "phase = ?",
+        "id = ?",
+        ("night", game_id)
+    )
+    players = [
+        player
+        for player in db.fetchall("werewolf_players")
+        if player[1] == game_id
+    ]
+
+    assign_roles(
+        db,
+        players,
+        werewolves,
+        seers,
+        doctors
+    )
     return True
