e632fb3306bf5ddca052de5bfe354a68a82b3f14
amri4 <amri.supreme123@gmail.com>
2026-09-08 22:09:24 +0300

    update york/systems/werewolf/buttons/join.py

--- york/systems/werewolf/buttons/join.py
+++ york/systems/werewolf/buttons/join.py
@@ -1,10 +1,34 @@
 import discord
+import mycord
+
+db = mycord.DB()
 
 class JoinButton(discord.ui.Button):
-    def __init__(self, gane_id):
+    def __init__(self, game_id):
         super().__init__(
             label="Join",
             emoji="➕️",
             style=discord.ButtonStyle.success
         )
         self.game_id = game_id
+
+    async def join_callback(
+        self,
+        interaction: discord.Interaction
+    ):
+        player_exists = db.exists(
+            "werewolf_players",
+            "game_id = ? AND user_id = ?",
+            (self.game_id, interaction.user.id)
+        )
+        if player_exists:
+            await interaction.response.send_message(
+                "🙄 Tch, you are already in this game",
+                ephemeral=True
+            )
+            return
+        db.insert(
+            "werewolf_players",
+            "game_id, user_id",
+            (self.game_id, interaction.user.id)
+        )
