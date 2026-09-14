3a510aba7aa3afdf7224b70ccf1673b9306a2dac
amri4 <amri.supreme123@gmail.com>
2026-09-14 12:53:29 +0300

    create edison/bot.py

---
+++ edison/bot.py
@@ -0,0 +1,27 @@
+import os
+import discord
+from discord.ext import commands
+from pathlib import Path
+
+
+class Bot(commands.Bot):
+
+    def __init__(self):
+        self.bot_name = Path(__file__).parent.name
+
+        super().__init__(
+            command_prefix="Atlas ",
+            intents=discord.Intents.all(),
+            help_command=None,
+            application_id=int(os.getenv("ATLAS_APPLICATION_ID"))
+        )
+
+    async def setup_hook(self):
+        await self.tree.sync()
+        print(f"[{self.bot_name}] Slash commands synced.")
+    
+    async def on_ready(self):
+        print(f"[{self.bot_name}] Logged in as {self.user}")
+
+
+bot = Bot()
