6e76208b98f4fff2593608e1ed1461ca562647c5
amri4 <amri.supreme123@gmail.com>
2026-09-16 02:55:05 +0300

    update lilith/systems/strikes/tables.py

--- lilith/systems/strikes/tables.py
+++ lilith/systems/strikes/tables.py
@@ -8,6 +8,7 @@ db.create_table(
     guild_id INTEGER NOT NULL,
     user_id INTEGER NOT NULL,
     strikes INTEGER NOT NULL DEFAULT 0,
+    prison_until INTEGER,
     PRIMARY KEY (guild_id, user_id)
     """
 )
