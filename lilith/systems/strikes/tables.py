318c53b4d174c7c4e14994942181b7e30e1a3adc
amri4 <amri.supreme123@gmail.com>
2026-09-16 02:54:33 +0300

    update lilith/systems/strikes/tables.py

---
+++ lilith/systems/strikes/tables.py
@@ -0,0 +1,13 @@
+import mycord
+
+db = mycord.DB()
+
+db.create_table(
+    "strikes",
+    """
+    guild_id INTEGER NOT NULL,
+    user_id INTEGER NOT NULL,
+    strikes INTEGER NOT NULL DEFAULT 0,
+    PRIMARY KEY (guild_id, user_id)
+    """
+)
