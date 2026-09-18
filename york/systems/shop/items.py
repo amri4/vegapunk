81d27f216fb8da10c787482dc67a54826892c33a
amri4 <amri.supreme123@gmail.com>
2026-09-19 01:21:39 +0300

    update york/systems/shop/items.py

--- york/systems/shop/items.py
+++ york/systems/shop/items.py
@@ -5,7 +5,6 @@ from .functions.items import bounty
 SHOP_ITEMS = [
     {
         "id": "bounty",
-        "channel_id": SHOP_CHANNEL_ID,
         "title": "Bounty",
         "description": "Exchange berries for bounty.",
         "price": 100,
