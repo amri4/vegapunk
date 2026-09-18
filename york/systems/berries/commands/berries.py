cd9514f65ed7f430ce9496079e7524d98b94a472
amri4 <amri.supreme123@gmail.com>
2026-09-18 13:34:55 +0300

    update york/systems/berries/commands/berries.py

---
+++ york/systems/berries/commands/berries.py
@@ -0,0 +1,21 @@
+import discord
+from discord import app_commands
+
+from ..functions.berries import get_berries
+
+
+@app_commands.command(
+    name="berries",
+    description="Check your berry balance."
+)
+async def berries(
+    interaction: discord.Interaction
+):
+    amount = get_berries(
+        interaction.guild.id,
+        interaction.user.id
+    )
+
+    await interaction.response.send_message(
+        f"🍓 You have **{amount:,} berries**."
+    )
