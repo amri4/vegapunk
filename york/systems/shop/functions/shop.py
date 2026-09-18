f3535393ae95ea94b728b00a811a24fb5e727dc3
amri4 <amri.supreme123@gmail.com>
2026-09-19 00:55:38 +0300

    update york/systems/shop/functions/shop.py

--- york/systems/shop/functions/shop.py
+++ york/systems/shop/functions/shop.py
@@ -5,49 +5,24 @@ db = mycord.DB()
 
 
 def add_item(
-    title,
-    description,
-    price,
+    item_id,
     channel_id,
-    reward_type
+    message_id
 ):
     db.insert(
         "shop",
-        "title, description, price, channel_id, message_id, reward_type",
+        "item_id, channel_id, message_id",
         (
-            title,
-            description,
-            price,
+            item_id,
             channel_id,
-            0,
-            reward_type
+            message_id
         )
     )
 
-    item = db.fetchone(
-        "shop",
-        """
-        title = ?
-        AND description = ?
-        AND price = ?
-        AND channel_id = ?
-        AND message_id = ?
-        AND reward_type = ?
-        """,
-        (
-            title,
-            description,
-            price,
-            channel_id,
-            0,
-            reward_type
-        )
-    )
 
-    return item[0]
-
-
-def get_item(item_id):
+def get_item(
+    item_id
+):
     return db.fetchone(
         "shop",
         "item_id = ?",
@@ -76,7 +51,9 @@ def update_message_id(
     )
 
 
-def remove_item(item_id):
+def remove_item(
+    item_id
+):
     db.delete(
         "shop",
         "item_id = ?",
