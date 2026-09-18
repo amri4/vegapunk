aa6d0dd9af187503df7f9f8a3d3ee761e2b30835
amri4 <amri.supreme123@gmail.com>
2026-09-19 01:23:48 +0300

    update york/systems/shop/functions/shop.py

--- york/systems/shop/functions/shop.py
+++ york/systems/shop/functions/shop.py
@@ -4,44 +4,95 @@ import mycord
 db = mycord.DB()
 
 
-def add_item(item_id, channel_id, message_id):
+def set_shop_channel(guild_id, channel_id):
+    db.insert_replace(
+        "shop",
+        "item_id, guild_id, channel_id, message_id",
+        (
+            "__channel__",
+            guild_id,
+            channel_id,
+            0
+        )
+    )
+
+
+def get_shop_channel(guild_id):
+    result = db.fetchone(
+        "shop",
+        "item_id = ? AND guild_id = ?",
+        (
+            "__channel__",
+            guild_id
+        )
+    )
+
+    if result is None:
+        return None
+
+    return result[2]
+
+
+def add_item(
+    item_id,
+    guild_id,
+    channel_id,
+    message_id
+):
     db.insert(
         "shop",
-        "item_id, channel_id, message_id",
+        "item_id, guild_id, channel_id, message_id",
         (
             item_id,
+            guild_id,
             channel_id,
             message_id
         )
     )
 
 
-def get_item(item_id):
+def get_item(item_id, guild_id):
     return db.fetchone(
         "shop",
-        "item_id = ?",
-        (item_id,)
+        "item_id = ? AND guild_id = ?",
+        (
+            item_id,
+            guild_id
+        )
     )
 
 
-def get_items():
+def get_items(guild_id):
     return db.fetchall(
-        "shop"
+        "shop",
+        "guild_id = ?",
+        (guild_id,)
     )
 
 
-def update_message_id(item_id, message_id):
+def update_message_id(
+    item_id,
+    guild_id,
+    message_id
+):
     db.update(
         "shop",
         "message_id = ?",
-        "item_id = ?",
-        (message_id, item_id)
+        "item_id = ? AND guild_id = ?",
+        (
+            message_id,
+            item_id,
+            guild_id
+        )
     )
 
 
-def remove_item(item_id):
+def remove_item(item_id, guild_id):
     db.delete(
         "shop",
-        "item_id = ?",
-        (item_id,)
+        "item_id = ? AND guild_id = ?",
+        (
+            item_id,
+            guild_id
+        )
     )
