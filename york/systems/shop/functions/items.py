e69342903ae04b1f9eb57882a976a1a2d48e649d
amri4 <amri.supreme123@gmail.com>
2026-09-19 00:36:51 +0300

    update york/systems/shop/functions/items.py

--- york/systems/shop/functions/items.py
+++ york/systems/shop/functions/items.py
@@ -2,8 +2,15 @@ from ....shaka.functions.bounty import add_bounty
 
 
 def bounty(interaction, amount):
+    bounty_amount = amount * 10
+
     add_bounty(
         interaction.guild.id,
         interaction.user.id,
-        amount
+        bounty_amount
+    )
+
+    return (
+        f"🏴‍☠️ You received **{bounty_amount:,} bounty** "
+        f"for **{amount:,} 🍓**!"
     )
