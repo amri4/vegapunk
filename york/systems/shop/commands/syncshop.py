d94c1f093c8958b7902bbe8b2bebdf228a7fe694
amri4 <amri.supreme123@gmail.com>
2026-09-19 01:26:09 +0300

    update york/systems/shop/commands/syncshop.py

--- york/systems/shop/commands/syncshop.py
+++ york/systems/shop/commands/syncshop.py
@@ -6,12 +6,17 @@ from ..functions.sync import sync_shop
 
 @app_commands.command(
     name="syncshop",
-    description="Sync the shop with its current items."
+    description="Sync the shop."
 )
-@app_commands.default_permissions(manage_guild=True)
-async def syncshop(interaction: discord.Interaction):
+@app_commands.default_permissions(
+    manage_guild=True
+)
+async def syncshop(
+    interaction: discord.Interaction
+):
     await sync_shop(
-        interaction.client
+        interaction.client,
+        interaction.guild
     )
 
     await interaction.response.send_message(
