622c99611c5806301b314d6f0068076968711e8e
amri4 <amri.supreme123@gmail.com>
2026-09-19 00:58:02 +0300

    update york/systems/shop/views/confirm.py

--- york/systems/shop/views/confirm.py
+++ york/systems/shop/views/confirm.py
@@ -1,6 +1,5 @@
 import discord
 
-from ..functions.shop import get_item
 from ..buttons.confirm import ConfirmButton
 from ..buttons.cancel import CancelButton
 
@@ -9,19 +8,19 @@ class ConfirmView(discord.ui.View):
 
     def __init__(
         self,
-        item_id,
+        item,
         user_id
     ):
         super().__init__(
             timeout=60
         )
 
-        self.item_id = item_id
+        self.item = item
         self.user_id = user_id
 
         self.add_item(
             ConfirmButton(
-                item_id,
+                item,
                 user_id
             )
         )
@@ -34,16 +33,12 @@ class ConfirmView(discord.ui.View):
 
 
 def confirmation_embed(item):
-    title = item[1]
-    description = item[2]
-    price = item[3]
-
     return discord.Embed(
         title="🛒 Confirm Purchase",
         description=(
-            f"**{title}**\n\n"
-            f"{description}\n\n"
-            f"**Price:** {price:,} 🍓\n\n"
+            f"**{item['title']}**\n\n"
+            f"{item['description']}\n\n"
+            f"**Price:** {item['price']:,} <:berries:1550458400800776344>\n\n"
             "Are you sure you want to buy this?"
         )
     )
