ea86cb8e99ac9b3954ed02d52d42c0601c15a75d
amri4 <amri.supreme123@gmail.com>
2026-09-19 00:41:01 +0300

    update york/systems/shop/buttons/cancel.py

---
+++ york/systems/shop/buttons/cancel.py
@@ -0,0 +1,32 @@
+import discord
+
+
+class CancelButton(discord.ui.Button):
+
+    def __init__(
+        self,
+        user_id
+    ):
+        super().__init__(
+            label="Cancel",
+            style=discord.ButtonStyle.danger
+        )
+
+        self.user_id = user_id
+
+    async def callback(
+        self,
+        interaction: discord.Interaction
+    ):
+        if interaction.user.id != self.user_id:
+            await interaction.response.send_message(
+                "❌ This isn't your purchase.",
+                ephemeral=True
+            )
+            return
+
+        await interaction.response.edit_message(
+            content="❌ Purchase cancelled.",
+            embed=None,
+            view=None
+        )
