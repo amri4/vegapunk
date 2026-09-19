69b17b512e991a45e7822b8c148ae56a53fe921d
amri4 <amri.supreme123@gmail.com>
2026-09-19 09:09:20 +0300

    update york/systems/shop/commands/syncshop.py

--- york/systems/shop/commands/syncshop.py
+++ york/systems/shop/commands/syncshop.py
@@ -8,23 +8,24 @@ from ..functions.sync import sync_shop
     name="syncshop",
     description="Sync the shop."
 )
-@app_commands.default_permissions(
-    manage_guild=True
-)
-async def syncshop(
-    interaction: discord.Interaction
-):
-    await sync_shop(
-        interaction.client,
-        interaction.guild
-    )
+@app_commands.default_permissions(manage_guild=True)
+async def syncshop(interaction: discord.Interaction):
+    try:
+        await sync_shop(
+            interaction.client,
+            interaction.guild
+        )
 
-    await interaction.response.send_message(
-        "✅ Shop synced successfully."
-    )
+        await interaction.response.send_message(
+            "✅ Shop synced successfully."
+        )
+
+    except Exception as error:
+        await interaction.response.send_message(
+            f"❌ Error: `{error}`",
+            ephemeral=True
+        )
 
 
 def setup(bot):
-    bot.tree.add_command(
-        syncshop
-    )
+    bot.tree.add_command(syncshop)
