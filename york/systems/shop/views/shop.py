532853f7da9f2fb7998f7e3ecf4975380da78374
amri4 <amri.supreme123@gmail.com>
2026-09-19 08:58:52 +0300

    update york/systems/shop/views/shop.py

--- york/systems/shop/views/shop.py
+++ york/systems/shop/views/shop.py
@@ -2,15 +2,11 @@ import discord
 
 from ..buttons.buy import BuyButton
 from ..items import SHOP_ITEMS
-from ..functions.shop import get_items
 
 
 class ShopView(discord.ui.View):
-
     def __init__(self, item):
-        super().__init__(
-            timeout=None
-        )
+        super().__init__(timeout=None)
 
         self.add_item(
             BuyButton(item)
@@ -18,15 +14,7 @@ class ShopView(discord.ui.View):
 
 
 def setup(bot):
-    saved_items = get_items()
-
-    saved_item_ids = {
-        item[0]
-        for item in saved_items
-    }
-
     for item in SHOP_ITEMS:
-        if item["id"] in saved_item_ids:
-            bot.add_view(
-                ShopView(item)
-            )
+        bot.add_view(
+            ShopView(item)
+        )
