380d154ba090c070919fc62822eafedf7148f287
amri4 <amri.supreme123@gmail.com>
2026-09-19 09:38:53 +0300

    update york/systems/shop/functions/items.py

--- york/systems/shop/functions/items.py
+++ york/systems/shop/functions/items.py
@@ -1,11 +1,37 @@
 from shaka.systems.bounty.functions.add_bounty import add_bounty
 
+from ..berries.functions.berries import (
+    get_berries,
+    remove_berries
+)
+
+
 def bounty(interaction, amount):
+    guild_id = interaction.guild.id
+    user_id = interaction.user.id
+
+    berries = get_berries(
+        guild_id,
+        user_id
+    )
+
+    if berries < amount:
+        return (
+            f"❌ You only have **{berries:,} <:berries:1550458400800776344>** "
+            f"but you're trying to spend **{amount:,} <:berries:1550458400800776344>**."
+        )
+
+    remove_berries(
+        guild_id,
+        user_id,
+        amount
+    )
+
     bounty_amount = amount * 10
 
     add_bounty(
-        interaction.guild.id,
-        interaction.user.id,
+        guild_id,
+        user_id,
         bounty_amount
     )
 
