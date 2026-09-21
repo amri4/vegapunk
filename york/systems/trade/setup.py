e1a852ebeb85b5378f862eecb80ad23226d90c86
amri4 <amri.supreme123@gmail.com>
2026-09-21 15:56:10 +0300

    update york/systems/trade/setup.py

---
+++ york/systems/trade/setup.py
@@ -0,0 +1,22 @@
+from ..functions.trades import get_trades
+from ..views.respond import TradeRespondView
+from ..views.trade import TradeView
+
+
+def setup(bot):
+    trades = get_trades()
+
+    for trade in trades:
+
+        trade_id = trade[0]
+        status = trade[4]
+
+        if status == "pending":
+            bot.add_view(
+                TradeRespondView(trade_id)
+            )
+
+        elif status == "accepted":
+            bot.add_view(
+                TradeView(trade_id)
+            )
