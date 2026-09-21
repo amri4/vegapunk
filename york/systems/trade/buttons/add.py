dff81d0d2598fe8412fab2020dcc36e7f20105bf
amri4 <amri.supreme123@gmail.com>
2026-09-21 13:41:45 +0300

    update york/systems/trade/buttons/add.py

---
+++ york/systems/trade/buttons/add.py
@@ -0,0 +1,23 @@
+import discord
+
+from ..modals.add import AddItemModal
+
+
+class AddItemButton(discord.ui.Button):
+    def __init__(self, trade_id):
+        super().__init__(
+            label="Add Item",
+            style=discord.ButtonStyle.primary
+        )
+
+        self.trade_id = trade_id
+
+    async def callback(
+        self,
+        interaction: discord.Interaction
+    ):
+        await interaction.response.send_modal(
+            AddItemModal(
+                self.trade_id
+            )
+        )
