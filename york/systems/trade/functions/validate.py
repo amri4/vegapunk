a21f1fb59a4fbef1ce8bc2adc650d0d3335707b6
amri4 <amri.supreme123@gmail.com>
2026-09-21 16:01:30 +0300

    update york/systems/trade/functions/validate.py

--- york/systems/trade/functions/validate.py
+++ york/systems/trade/functions/validate.py
@@ -1,5 +1,6 @@
 from ...inventory.functions.inventory import get_amount
 from ...berries.functions.berries import get_berries
+from ...devilfruits.functions.fruits import get_fruit
 
 from .offers import get_offers
 
@@ -14,21 +15,36 @@ def validate_trade(
     guild_id = trade[1]
 
     for offer in offers:
+
         user_id = offer[1]
         item_id = offer[2]
         amount = offer[3]
 
         if item_id == BERRIES_ITEM_ID:
+
             current = get_berries(
                 guild_id,
                 user_id
             )
+
         else:
+
             current = get_amount(
                 guild_id,
                 user_id,
                 item_id
             )
+
+            fruit = get_fruit(
+                item_id
+            )
+
+            if fruit is not None:
+
+                tradeable = fruit[4]
+
+                if not tradeable:
+                    return False
 
         if current < amount:
             return False
