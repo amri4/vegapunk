eb58f4aa081433e39a68cd327e1326e40ed06070
amri4 <amri.supreme123@gmail.com>
2026-09-21 13:50:18 +0300

    update york/systems/trade/functions/offer.py

--- york/systems/trade/functions/offer.py
+++ york/systems/trade/functions/offer.py
@@ -1,5 +1,13 @@
 from ...inventory.functions.inventory import get_amount
-from .offers import add_offer
+
+from .offers import (
+    add_offer,
+    get_offer,
+    remove_offer,
+    update_offer
+)
+
+from .confirmations import clear_confirmations
 
 
 def add_item_to_trade(
@@ -23,6 +31,51 @@ def add_item_to_trade(
         user_id,
         item_id,
         amount
+    )
+
+    clear_confirmations(
+        trade_id
+    )
+
+    return True
+
+
+def remove_item_from_trade(
+    trade_id,
+    user_id,
+    item_id,
+    amount
+):
+    offer = get_offer(
+        trade_id,
+        user_id,
+        item_id
+    )
+
+    if offer is None:
+        return False
+
+    current = offer[3]
+
+    if amount > current:
+        return False
+
+    if amount == current:
+        remove_offer(
+            trade_id,
+            user_id,
+            item_id
+        )
+    else:
+        update_offer(
+            trade_id,
+            user_id,
+            item_id,
+            current - amount
+        )
+
+    clear_confirmations(
+        trade_id
     )
 
     return True
